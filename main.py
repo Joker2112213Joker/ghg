7aa4920f9978605b95b3d26b7fac9f014a0990da
Joker2112213Joker <147646513+Joker2112213Joker@users.noreply.github.com>
2023-10-11 20:42:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__mairtytun__':
-    print_hi('Pyheliolo')
+    print_hi('Pyheliolssso')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
